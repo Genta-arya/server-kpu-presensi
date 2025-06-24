91518ff42b557198d42586a6ef1ef50142b9dbb7
GentaArya <mgentaarya@yahoo.com>
2025-06-24 11:37:35 +0700

    update

--- src/Controller/Absen.controller.js
+++ src/Controller/Absen.controller.js
@@ -1,16 +1,15 @@
 import { prisma } from "../Config/Prisma.js";
 
-
-
 export const createAbsen = async (req, res) => {
   try {
-    const { userId, img_ttd, status } = req.body;
+    const { userId, img_ttd, status, koordinat } = req.body;
 
-    if (!userId || !img_ttd) {
-      return res.status(400).json({ message: 'userId dan img_ttd wajib diisi' });
+    if (!userId || !img_ttd || !koordinat) {
+      return res
+        .status(400)
+        .json({ message: "Semua field wajib diisi" });
     }
 
-  
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
@@ -21,14 +20,14 @@ export const createAbsen = async (req, res) => {
       where: {
         userId,
         createdAt: {
-          gte: today,  // >= jam 00:00 hari ini
-          lt: besok    // < jam 00:00 besok
-        }
-      }
+          gte: today, // >= jam 00:00 hari ini
+          lt: besok, // < jam 00:00 besok
+        },
+      },
     });
 
     if (sudahAbsen) {
-      return res.status(409).json({ message: 'Kamu sudah absen hari ini!' });
+      return res.status(409).json({ message: "Kamu sudah absen hari ini!" });
     }
 
     // Simpan absen baru
@@ -36,13 +35,16 @@ export const createAbsen = async (req, res) => {
       data: {
         userId,
         img_ttd,
-        status: status || 'hadir',
+        koordinat: koordinat,
+        status: status || "hadir",
       },
     });
 
-    res.status(201).json({ message: 'Absen berhasil', data: absen });
+    res.status(201).json({ message: "Absen berhasil", data: absen });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Terjadi kesalahan', error: error.message });
+    res
+      .status(500)
+      .json({ message: "Terjadi kesalahan", error: error.message });
   }
 };
