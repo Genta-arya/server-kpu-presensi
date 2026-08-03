0b4eebc472d24967ec6e58855753347b271f535f
Genta-arya <mgentaaryap@gmail.com>
2026-08-03 11:10:53 +0700

    update origin

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import { createServer } from "http";
 import cors from "cors";
@@ -10,12 +9,28 @@ const app = express();
 const PORT = 8080;
 const httpServer = createServer(app);
 app.use(express.json());
+
+// Daftar domain yang diizinkan mengakses API Anda
+const allowedOrigins = [
+  "https://presensi.kpu-sekadau.my.id",
+  "https://pegawai.kpu-sekadau.my.id"
+];
+
 app.use(
   cors({
-    origin: true,
+    origin: function (origin, callback) {
+      // Izinkan request yang tidak memiliki origin (seperti Postman atau mobile app)
+      // atau jika origin ada di dalam daftar allowedOrigins
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use("/", webRoutes); 
 app.get("/api/cron/generate-absen", triggerAutoAbsen);
 
@@ -24,4 +39,3 @@ app.get("/api/cron/generate-absen", triggerAutoAbsen);
 httpServer.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
   });
-  
